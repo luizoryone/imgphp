91fbe60bd2c22b413f053985c7eaab301639d177
luizoryone <luizoryone@gmail.com>
2025-09-22 10:59:20 -0300

    add index.php and fiveserver.config.js files with initial PHP code and configuration

---
+++ firts22092025/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Inicial 22092025</title>
+</head>
+
+<body>
+    <h1>Olá, Mundo! Este é meu primeiro código PHP.</h1>
+    <?php
+        echo "<p>Este é um parágrafo gerado por PHP.</p>";
+        $nome = "João";
+        $idade = 25;
+        echo "<p>Meu nome é $nome e eu tenho $idade anos.</p>";
+    ?>
+
+</body>
+
+</html>

5e415d7211d7cfa76a17e94c449ba5a3c34f05e6
Luiz Oryone <luizoryone@gmail.com>
2025-09-22 12:14:52 -0300

    Adicionados arquivos da pasta da biblioteca
    Arquivo da biblioteca adiconados

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let btnDoH1 = document.getElementById("btnRedH1");
+let textoDoH1 = document.getElementById("btnBlueH1");
+let rox1 =document.getElementById("roxo").style.backgroundColor ="red";
+
+
+textoDoH1.addEventListener("click",()=>{
+  textoDoH1.innerText = "Texto com alteração";
+});
+
